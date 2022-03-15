044d112b628af3639c4d665125b18b6c6b40b3eb
Bilal Ahmed <92183691+csbilalahmed@users.noreply.github.com>
2022-03-15 14:15:32 -0500

    fixes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,14 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Http\Request;
-
+use App\Models\GuidanceReport;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()  
     {
         return view('dashboard');
+    }
+
+    public function teamOneGraph()
+    {
+        $datas = GuidanceReport::select('rea_sign_up')->whereHas('user', function($query){
+            $query->where('team_id',1);
+        })->whereYear('created_at', date('Y'))
+        ->groupBy(DB::raw("Day(created_at)"))
+        ->pluck('rea_sign_up');
+        $months = GuidanceReport::select(DB::raw('Month(created_at) as month'))->whereHas('user', function($query){
+            $query->where('team_id',1);
+        })->whereYear('created_at', date('Y'))
+        ->groupBy(DB::raw("Month(created_at)"))
+        ->pluck('month');
+        return view('dashboard', compact('datas','months'));
     }
 
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,67 @@
 @extends('layouts.app')
 @section('title', 'Dashboard')
 @section('content')
+<div class="row">
+    <div class="col-md-3">
+        <div id="chart_one"></div>
+    </div>
+    <div class="col-md-3">
+        <div id="chart_two"></div>
+    </div>
+    <div class="col-md-3">
+        <div id="chart_three"></div>
+    </div>
+  </div>
+@endsection
+@section('scripts')
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script>
+   var data = <?php echo json_encode($datas)?>;
+   var month = <?php echo json_encode($months)?>;
+    Highcharts.chart('chart_one', {
+        title: {
+            text: 'Team One'
+        },
+
+        xAxis: {
+            categories: month
+        },
+        yAxis: {
+            title: {
+                text: 'REA SIGNUP'
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+        plotOptions: {
+            series: {
+                allowPointSelect: true
+            }
+        },
+        series: [{
+            name: 'REA SIGNUP',
+            data: data
+        }],
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
+            }]
+        }
+    });
+
+
+
+</script>
 @endsection
