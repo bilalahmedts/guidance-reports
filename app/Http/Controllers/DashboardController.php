62b683cc8e22e9e929ab65ab7c59253d3f59e6b3
Bilal Ahmed <92183691+csbilalahmed@users.noreply.github.com>
2022-03-17 07:37:44 -0500

    graphs completed.....

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,14 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Carbon\Carbon;
-use App\Models\User;
-use App\Models\Category;
 use App\Models\GuidanceReport;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
 class DashboardController extends Controller
 {
     public function index()
@@ -26,24 +20,185 @@ class DashboardController extends Controller
         ->groupBy(DB::raw('Date(created_at)'))
         ->pluck('rea');
 
+        $team_one_months = GuidanceReport::select(DB::raw('MONTHNAME(created_at) as month'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',1);
+        })->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('MONTHNAME(created_at)'))
+        ->orderBy('created_at','asc')
+        ->pluck('month')->toArray();
+
+        $team_one_data_monthly = GuidanceReport::select(DB::raw('SUM(rea_sign_up) as rea'))->whereHas('user', function($query){
+            $query->where('team_id',1);
+        })->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('MONTHNAME(created_at)'))
+        ->orderBy('created_at','asc')
+        ->pluck('rea')->toArray();
+
+        $team_two_dates = GuidanceReport::select(DB::raw('Date(created_at) as date'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('Date(created_at)'))
+        ->pluck('date');
+
+        $team_two_online_pq_transfer_per_day = GuidanceReport::select(DB::raw('SUM(transfer_per_day) as transfer'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->where('categories_id',1)
+        ->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('Date(created_at)'))
+        ->pluck('transfer');
+
+        $team_two_online_pq_call_per_day = GuidanceReport::select(DB::raw('SUM(call_per_day) as calls'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->where('categories_id',1)
+        ->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('Date(created_at)'))
+        ->pluck('calls');
+
+        $team_two_fallout_transfer_per_day = GuidanceReport::select(DB::raw('SUM(transfer_per_day) as transfer'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->where('categories_id',2)
+        ->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('Date(created_at)'))
+        ->pluck('transfer');
+
+        $team_two_fallout_call_per_day = GuidanceReport::select(DB::raw('SUM(call_per_day) as calls'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->where('categories_id',2)
+        ->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('Date(created_at)'))
+        ->pluck('calls');
+
+        $team_two_others_transfer_per_day = GuidanceReport::select(DB::raw('SUM(transfer_per_day) as transfer'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->where('categories_id',3)
+        ->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('Date(created_at)'))
+        ->pluck('transfer');
+
+        $team_two_others_call_per_day = GuidanceReport::select(DB::raw('SUM(call_per_day) as calls'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->where('categories_id',3)
+        ->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('Date(created_at)'))
+        ->pluck('calls');
+
+        $team_two_months = GuidanceReport::select(DB::raw('MONTHNAME(created_at) as month'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('MONTHNAME(created_at)'))
+        ->orderBy('created_at','asc')
+        ->pluck('month')->toArray();
+
+        $team_two_online_pq_transfer_per_day_monthly = GuidanceReport::select(DB::raw('SUM(transfer_per_day) as transfer'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->where('categories_id',1)
+        ->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('MONTHNAME(created_at)'))
+        ->orderBy('created_at','asc')
+        ->pluck('transfer')->toArray();
+
+        $team_two_online_pq_call_per_day_monthly = GuidanceReport::select(DB::raw('SUM(call_per_day) as calls'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->where('categories_id',1)
+        ->groupBy(DB::raw('MONTHNAME(created_at)'))
+        ->orderBy('created_at','asc')
+        ->pluck('calls')->toArray();
+
+        $team_two_fallout_transfer_per_day_monthly = GuidanceReport::select(DB::raw('SUM(transfer_per_day) as transfer'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->where('categories_id',2)
+        ->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('MONTHNAME(created_at)'))
+        ->orderBy('created_at','asc')
+        ->pluck('transfer')->toArray();
+
+        $team_two_fallout_call_per_day_monthly = GuidanceReport::select(DB::raw('SUM(call_per_day) as calls'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->where('categories_id',2)
+        ->groupBy(DB::raw('MONTHNAME(created_at)'))
+        ->orderBy('created_at','asc')
+        ->pluck('calls')->toArray();
+
+        $team_two_others_transfer_per_day_monthly = GuidanceReport::select(DB::raw('SUM(transfer_per_day) as transfer'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->where('categories_id',3)
+        ->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('MONTHNAME(created_at)'))
+        ->orderBy('created_at','asc')
+        ->pluck('transfer')->toArray();
+
+        $team_two_others_call_per_day_monthly = GuidanceReport::select(DB::raw('SUM(call_per_day) as calls'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',2);
+        })->where('categories_id',3)
+        ->groupBy(DB::raw('MONTHNAME(created_at)'))
+        ->orderBy('created_at','asc')
+        ->pluck('calls')->toArray();
+        
         $team_three_dates = GuidanceReport::select(DB::raw('Date(created_at) as date'))
         ->whereHas('user', function($query){
             $query->where('team_id',3);
         })->whereYear('created_at',date('Y'))
         ->groupBy(DB::raw('Date(created_at)'))
         ->pluck('date');
+
         $team_three_tbd_assigned_data = GuidanceReport::select(DB::raw('SUM(tbd_assigned) as tbd'))->whereHas('user', function($query){
             $query->where('team_id',3);
         })->whereYear('created_at',date('Y'))
         ->groupBy(DB::raw('Date(created_at)'))
         ->pluck('tbd');
+
         $team_three_no_of_matches_data = GuidanceReport::select(DB::raw('SUM(no_of_matches) as matches'))->whereHas('user', function($query){
             $query->where('team_id',3);
         })->whereYear('created_at',date('Y'))
         ->groupBy(DB::raw('Date(created_at)'))
         ->pluck('matches');
 
-        return view('dashboard', compact('team_one_dates','team_one_data','team_three_dates','team_three_tbd_assigned_data','team_three_no_of_matches_data'));
+        $team_three_months = GuidanceReport::select(DB::raw('MONTHNAME(created_at) as month'))
+        ->whereHas('user', function($query){
+            $query->where('team_id',3);
+        })->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('MONTHNAME(created_at)'))
+        ->orderBy('created_at','asc')
+        ->pluck('month')->toArray();
+
+        $team_three_tbd_assigned_data_monthly = GuidanceReport::select(DB::raw('SUM(tbd_assigned) as tbd'))->whereHas('user', function($query){
+            $query->where('team_id',3);
+        })->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('MONTHNAME(created_at)'))
+        ->orderBy('created_at','asc')
+        ->pluck('tbd')->toArray();
+
+        $team_three_no_of_matches_data_monthly = GuidanceReport::select(DB::raw('SUM(no_of_matches) as matches'))->whereHas('user', function($query){
+            $query->where('team_id',3);
+        })->whereYear('created_at',date('Y'))
+        ->groupBy(DB::raw('MONTHNAME(created_at)'))
+        ->orderBy('created_at','asc')
+        ->pluck('matches')->toArray();
+
+        return view('dashboard', compact('team_one_dates','team_one_data','team_one_months','team_one_data_monthly',
+                    'team_two_dates','team_two_online_pq_transfer_per_day','team_two_online_pq_call_per_day',
+                    'team_two_fallout_transfer_per_day','team_two_fallout_call_per_day',
+                    'team_two_others_transfer_per_day','team_two_others_call_per_day','team_two_months',
+                    'team_two_online_pq_transfer_per_day_monthly','team_two_online_pq_call_per_day_monthly',
+                    'team_two_fallout_transfer_per_day_monthly','team_two_fallout_call_per_day_monthly',
+                    'team_two_others_transfer_per_day_monthly','team_two_others_call_per_day_monthly',
+                    'team_three_dates','team_three_tbd_assigned_data','team_three_no_of_matches_data','team_three_months',
+                    'team_three_tbd_assigned_data_monthly','team_three_no_of_matches_data_monthly'));
     }
 
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,19 @@
             <div id="chart-date-one"></div>
         </div>
         <div class="col-md-6">
-            <div id="chart-date-one"></div>
+            <div id="chart-month-one"></div>
+        </div>
+        <div class="col-md-6">
+            <div id="chart-date-team-two"></div>
+        </div>
+        <div class="col-md-6">
+            <div id="chart-month-team-two"></div>
         </div>
         <div class="col-md-6">
             <div id="chart-date-team-three"></div>
+        </div>
+        <div class="col-md-6">
+            <div id="chart-month-team-three"></div>
         </div>
     </div>
 @endsection
@@ -18,9 +27,9 @@
     <script>
         var data = [
             @foreach ($team_one_data as $team_one)
-                        {{ $team_one }},
-                    @endforeach
-                ];
+                {{ $team_one }},
+            @endforeach
+        ];
         var date = [
             @foreach ($team_one_dates as $date)
                 "{{ $date }}",
@@ -28,7 +37,7 @@
         ];
         Highcharts.chart('chart-date-one', {
             title: {
-                text: 'Team One'
+                text: 'Team One (Datewise)'
             },
             xAxis: {
                 categories: date
@@ -69,16 +78,272 @@
         });
     </script>
     <script>
+        var data = [
+            @foreach ($team_one_data_monthly as $team_one)
+                {{ $team_one }},
+            @endforeach
+        ];
+        var month = [
+            @foreach ($team_one_months as $month)
+                "{{ $month }}",
+            @endforeach
+        ];
+        Highcharts.chart('chart-month-one', {
+            title: {
+                text: 'Team One (Monthly)'
+            },
+            xAxis: {
+                categories: month
+            },
+            yAxis: {
+                title: {
+                    text: ''
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+            },
+            plotOptions: {
+                series: {
+                    allowPointSelect: true
+                }
+            },
+            series: [{
+                name: 'REA SIGNUP',
+                data: data
+            }],
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        legend: {
+                            layout: 'horizontal',
+                            align: 'center',
+                            verticalAlign: 'bottom'
+                        }
+                    }
+                }]
+            }
+        });
+    </script>
+        <script>
+            var online_pq_transfer = [
+                @foreach ($team_two_online_pq_transfer_per_day as $transfer)
+                    {{ $transfer }},
+                @endforeach
+            ];
+            var online_pq_call = [
+                @foreach ($team_two_online_pq_call_per_day as $calls)
+                    {{ $calls }},
+                @endforeach
+            ];
+            var fallout_transfer = [
+                @foreach ($team_two_fallout_transfer_per_day as $fallout_transfer)
+                    {{ $fallout_transfer }},
+                @endforeach
+            ];
+            var fallout_call = [
+                @foreach ($team_two_fallout_call_per_day as $fallout_calls)
+                    {{ $fallout_calls }},
+                @endforeach
+            ];
+            var others_transfer = [
+                @foreach ($team_two_others_transfer_per_day as $other_transfer)
+                    {{ $other_transfer }},
+                @endforeach
+            ];
+            var others_call = [
+                @foreach ($team_two_others_call_per_day as $other_calls)
+                    {{ $other_calls }},
+                @endforeach
+            ];
+            var date = [
+                @foreach ($team_two_dates as $date)
+                    "{{ $date }}",
+                @endforeach
+            ];
+            Highcharts.chart('chart-date-team-two', {
+                title: {
+                    text: 'Team Two (Datewise)'
+                },
+                xAxis: {
+                    categories: date
+                },
+                yAxis: {
+                    title: {
+                        text: ''
+                    }
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle'
+                },
+                plotOptions: {
+                    series: {
+                        allowPointSelect: true
+                    }
+                },
+                series: [{
+                        name: 'Online PQ Transfer Per Day',
+                        data: online_pq_transfer
+                    },
+                    {
+                        name: 'Online PQ Call Per Day',
+                        data: online_pq_call
+                    },
+                    {
+                        name: 'Fallout Transfer Per Day',
+                        data: fallout_transfer
+                    },
+                    {
+                        name: 'Fallout Call Per Day',
+                        data: fallout_call
+                    },
+                    {
+                        name: 'Others Transfer Per Day',
+                        data: others_transfer
+                    },
+                    {
+                        name: 'Others Call Per Day',
+                        data: others_call
+                    },
+                ],
+                responsive: {
+                    rules: [{
+                        condition: {
+                            maxWidth: 500
+                        },
+                        chartOptions: {
+                            legend: {
+                                layout: 'horizontal',
+                                align: 'center',
+                                verticalAlign: 'bottom'
+                            }
+                        }
+                    }]
+                }
+            });
+        </script>
+    <script>
+        var online_pq_transfer = [
+            @foreach ($team_two_online_pq_transfer_per_day_monthly as $pq_monthly)
+                {{ $pq_monthly }},
+            @endforeach
+        ];
+        var online_pq_calls = [
+            @foreach ($team_two_online_pq_call_per_day_monthly as $pq_calls_monthly)
+                {{ $pq_calls_monthly }},
+            @endforeach
+        ];
+
+        var fallout_transfers_monthly = [
+            @foreach ($team_two_fallout_transfer_per_day_monthly as $fallout_monthly)
+                {{ $fallout_monthly }},
+            @endforeach
+        ];
+        var fallout_calls_monthly = [
+            @foreach ($team_two_fallout_call_per_day_monthly as $fallout_monthly)
+                {{ $fallout_monthly }},
+            @endforeach
+        ];
+
+        var others_transfer_monthly = [
+            @foreach ($team_two_others_transfer_per_day_monthly as $other_transfer)
+                {{ $other_transfer }},
+            @endforeach
+        ];
+        var others_call_monthly = [
+            @foreach ($team_two_others_call_per_day_monthly as $other_calls)
+                {{ $other_calls }},
+            @endforeach
+        ];
+       
+        var month = [
+            @foreach ($team_two_months as $month)
+                "{{ $month }}",
+            @endforeach
+        ];
+        Highcharts.chart('chart-month-team-two', {
+            title: {
+                text: 'Team Two (Monthly)'
+            },
+            xAxis: {
+                categories: month
+            },
+            yAxis: {
+                title: {
+                    text: ''
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+            },
+            plotOptions: {
+                series: {
+                    allowPointSelect: true
+                }
+            },
+            series: [
+                {
+                name: 'Online PQ Transfer',
+                data: online_pq_transfer
+            },
+            {
+                name: 'Online PQ Calls',
+                data: online_pq_calls
+            },
+            {
+                name: 'Fallout Transfer',
+                data: fallout_transfers_monthly
+            },
+            {
+                name: 'Fallout Calls',
+                data: fallout_calls_monthly
+            },
+            {
+                name: 'Others Transfer',
+                data: others_transfer_monthly
+            },
+            {
+                name: 'Others Calls',
+                data: others_call_monthly
+            },
+
+        ],
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        legend: {
+                            layout: 'horizontal',
+                            align: 'center',
+                            verticalAlign: 'bottom'
+                        }
+                    }
+                }]
+            }
+        });
+    </script>
+    <script>
         var tbd_assigned = [
             @foreach ($team_three_tbd_assigned_data as $tbd)
-                        {{ $tbd }},
-                    @endforeach
-                ];
+                {{ $tbd }},
+            @endforeach
+        ];
         var no_of_matches = [
             @foreach ($team_three_no_of_matches_data as $matches)
-                        {{ $matches }},
-                    @endforeach
-                ];
+                {{ $matches }},
+            @endforeach
+        ];
         var date = [
             @foreach ($team_three_dates as $date)
                 "{{ $date }}",
@@ -86,7 +351,7 @@
         ];
         Highcharts.chart('chart-date-team-three', {
             title: {
-                text: 'Team Three'
+                text: 'Team Three (Datewise)'
             },
             xAxis: {
                 categories: date
@@ -106,16 +371,15 @@
                     allowPointSelect: true
                 }
             },
-            series: [
+            series: [{
+                    name: 'TBD Assigned',
+                    data: tbd_assigned
+                },
                 {
-                name: 'TBD Assigned',
-                data: tbd_assigned
-            },
-            {
-                name: 'Number of Matches',
-                data: no_of_matches
-            },
-        ],
+                    name: 'Number of Matches',
+                    data: no_of_matches
+                },
+            ],
             responsive: {
                 rules: [{
                     condition: {
@@ -132,4 +396,67 @@
             }
         });
     </script>
+        <script>
+            var tbd_assigned = [
+                @foreach ($team_three_tbd_assigned_data_monthly as $tbd)
+                    {{ $tbd }},
+                @endforeach
+            ];
+            var no_of_matches = [
+                @foreach ($team_three_no_of_matches_data_monthly as $matches)
+                    {{ $matches }},
+                @endforeach
+            ];
+            var month = [
+                @foreach ($team_three_months as $month)
+                    "{{ $month }}",
+                @endforeach
+            ];
+            Highcharts.chart('chart-month-team-three', {
+                title: {
+                    text: 'Team Three (Monthly)'
+                },
+                xAxis: {
+                    categories: month
+                },
+                yAxis: {
+                    title: {
+                        text: ''
+                    }
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle'
+                },
+                plotOptions: {
+                    series: {
+                        allowPointSelect: true
+                    }
+                },
+                series: [{
+                        name: 'TBD Assigned',
+                        data: tbd_assigned
+                    },
+                    {
+                        name: 'Number of Matches',
+                        data: no_of_matches
+                    },
+                ],
+                responsive: {
+                    rules: [{
+                        condition: {
+                            maxWidth: 500
+                        },
+                        chartOptions: {
+                            legend: {
+                                layout: 'horizontal',
+                                align: 'center',
+                                verticalAlign: 'bottom'
+                            }
+                        }
+                    }]
+                }
+            });
+        </script>
 @endsection
